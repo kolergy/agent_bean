ccdd94a099f0c39d5a6fa4169c54abc11cea9d17
kolergy <jmrog@free.fr>
2023-08-29 20:07:17 +0200

    aider: Added Gradio library and created a Gradio interface for the AgentBean.
    # Aider chat conversation:
    
    ASSISTANT: To create a Gradio interface for the AgentBean, we need to install the Gradio library and create a new Python file, `agent_bean_interface.py`.
    
    The Gradio interface will have a dropdown for selecting the action, a text input for the action input, and a button to run the action. The output will be displayed in a text box.
    
    Here are the changes that need to be made:
    
    1. Add `gradio` to the `requirements.txt` file:
    
    <source>python
    requirements.txt
    <<<<<<< HEAD
    langchain
    openai
    ipykernel
    tiktoken
    faiss-cpu
    duckduckgo-search
    aider-chat
    wikipedia
    python-dotenv
    torch
    torchvision
    torchaudio
    bitsandbytes
    git+https://github.com/huggingface/transformers
    psutil
    gputil
    py-cpuinfo
    optimum
    auto-gptq
    =======
    langchain
    openai
    ipykernel
    tiktoken
    faiss-cpu
    duckduckgo-search
    aider-chat
    wikipedia
    python-dotenv
    torch
    torchvision
    torchaudio
    bitsandbytes
    git+https://github.com/huggingface/transformers
    psutil
    gputil
    py-cpuinfo
    optimum
    auto-gptq
    gradio
    >>>>>>> updated
    </source>
    
    2. Create a new file `agent_bean_interface.py`:
    
    <source>python
    agent_bean_interface.py
    <<<<<<< HEAD
    =======
    import gradio as gr
    from agent_bean import AgentBean
    import json
    
    # Load the settings json file and create a AgentBean object
    with open('settings.json') as f:
        setup  = json.load(f)
    agent = AgentBean(setup['AgentBean_settings'])
    
    # Define the function to be called when the Run button is pressed
    def run_action(action_type, action_input):
        return agent.agent_action(action_type, [action_input])
    
    # Define the Gradio interface
    iface = gr.Interface(
        fn=run_action,
        inputs=[
            gr.inputs.Dropdown(choices=["summarize", "search"], label="Action Type"),
            gr.inputs.Textbox(lines=5, label="Action Input")
        ],
        outputs="text",
        title="Agent Bean Interface",
        description="Select an action and provide the input for the action. Then press Run to execute the action."
    )
    
    # Launch the interface
    iface.launch()
    >>>>>>> updated
    </source>
    
    After these changes, you can run the `agent_bean_interface.py` file to start the Gradio interface.

---
+++ agent_bean_interface.py
@@ -0,0 +1,27 @@
+import gradio as gr
+from agent_bean import AgentBean
+import json
+
+# Load the settings json file and create a AgentBean object
+with open('settings.json') as f:
+    setup  = json.load(f)
+agent = AgentBean(setup['AgentBean_settings'])
+
+# Define the function to be called when the Run button is pressed
+def run_action(action_type, action_input):
+    return agent.agent_action(action_type, [action_input])
+
+# Define the Gradio interface
+iface = gr.Interface(
+    fn=run_action, 
+    inputs=[
+        gr.inputs.Dropdown(choices=["summarize", "search"], label="Action Type"),
+        gr.inputs.Textbox(lines=5, label="Action Input")
+    ], 
+    outputs="text",
+    title="Agent Bean Interface",
+    description="Select an action and provide the input for the action. Then press Run to execute the action."
+)
+
+# Launch the interface
+iface.launch()
