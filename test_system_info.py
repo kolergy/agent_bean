b1387e7b3e91817d7256183dde84d5d5d9f82be7
kolergy <jmrog@free.fr>
2023-08-29 16:21:47 +0200

    aider: Generated a test file for `system_info.py`.
    # Aider chat conversation:
    
    USER: generate a test file for system_info.py
    
    ASSISTANT: To generate a test file for `system_info.py`, we will create a new file named `test_system_info.py`. This file will contain a series of unit tests to verify the functionality of the `SystemInfo` class.
    
    Here is the content for the new test file:
    
    ```python
    test_system_info.py
    <<<<<<< HEAD
    
    =======
    import unittest
    from system_info import SystemInfo
    
    class TestSystemInfo(unittest.TestCase):
        def setUp(self):
            self.system_info = SystemInfo()
    
        def test_cpu_info(self):
            cpu_info = self.system_info.get_cpu_info()
            self.assertIsNotNone(cpu_info)
            self.assertIn('brand', cpu_info)
            self.assertIn('cores', cpu_info)
            self.assertIn('ram_total', cpu_info)
            self.assertIn('ram_used', cpu_info)
    
        def test_gpu_info(self):
            gpu_info = self.system_info.get_gpu_info()
            self.assertIsNotNone(gpu_info)
            if gpu_info:
                self.assertIn('brand', gpu_info[0])
                self.assertIn('vram_total', gpu_info[0])
                self.assertIn('vram_used', gpu_info[0])
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> updated
    ```
    
    This test file contains two tests: `test_cpu_info` and `test_gpu_info`. These tests check that the `get_cpu_info` and `get_gpu_info` methods of the `SystemInfo` class return the expected information.

---
+++ test_system_info.py
@@ -0,0 +1,25 @@
+import unittest
+from system_info import SystemInfo
+
+class TestSystemInfo(unittest.TestCase):
+    def setUp(self):
+        self.system_info = SystemInfo()
+
+    def test_cpu_info(self):
+        cpu_info = self.system_info.get_cpu_info()
+        self.assertIsNotNone(cpu_info)
+        self.assertIn('brand', cpu_info)
+        self.assertIn('cores', cpu_info)
+        self.assertIn('ram_total', cpu_info)
+        self.assertIn('ram_used', cpu_info)
+
+    def test_gpu_info(self):
+        gpu_info = self.system_info.get_gpu_info()
+        self.assertIsNotNone(gpu_info)
+        if gpu_info:
+            self.assertIn('brand', gpu_info[0])
+            self.assertIn('vram_total', gpu_info[0])
+            self.assertIn('vram_used', gpu_info[0])
+
+if __name__ == '__main__':
+    unittest.main()
