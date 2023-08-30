f6584a91020ac305e92f95a1636895de45dec5d1
kolergy <jmrog@free.fr>
2023-08-30 13:18:57 +0200

    Refactor the code to improve readability and organization.

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -1,6 +1,9 @@
-import gradio as gr
-from agent_bean import AgentBean
 import json
+
+import gradio       as     gr
+
+from   agent_bean   import AgentBean
+
 
 # Load the settings json file and create a AgentBean object
 with open('settings.json') as f:
@@ -13,14 +16,14 @@ def run_action(action_type, action_input):
 
 # Define the Gradio interface
 iface = gr.Interface(
-    fn=run_action, 
-    inputs=[
+    fn          = run_action, 
+    inputs      = [
         gr.inputs.Dropdown(choices=["summarize", "search"], label="Action Type"),
         gr.inputs.Textbox(lines=5, label="Action Input")
     ], 
-    outputs="text",
-    title="Agent Bean Interface",
-    description="Select an action and provide the input for the action. Then press Run to execute the action."
+    outputs     = "text",
+    title       = "Agent Bean Interface",
+    description = "Select an action and provide the input for the action. Then press Run to execute the action."
 )
 
 # Launch the interface
