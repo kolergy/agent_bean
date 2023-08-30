353f610753d8fcf91b7f18f5eb4c9f6ec35281ef
kolergy <jmrog@free.fr>
2023-08-30 13:32:26 +0200

    aider: Replaced `gr.Interface` with `gr.Blocks` in `agent_bean_interface.py`.
    # Aider chat conversation:
    
    ASSISTANT: To replace `gr.Interface` with `gr.Blocks` in the `agent_bean_interface.py` file, we need to modify the way we define the interface.
    
    Here are the steps:
    
    1. Import `gradio.blocks` instead of `gradio.components`.
    2. Replace the `gr.Interface` instance with `gr.Blocks`.
    3. Define the inputs and outputs as blocks.
    4. Define the functions as blocks.
    
    Here is the *edit block* for these changes:
    
    ```python
    agent_bean_interface.py
    <<<<<<< HEAD
    import json
    
    import gradio       as     gr
    
    from   agent_bean   import AgentBean
    
    # Load the settings json file and create a AgentBean object
    with open('settings.json') as f:
        setup  = json.load(f)
    agent = AgentBean(setup['AgentBean_settings'])
    
    # Define the function to be called when the Run button is pressed
    def run_action(action_type, action_input):
        return agent.agent_action(action_type, [action_input])
    
    # Define the function to be called when the Load Document button is pressed
    def load_document(document_path):
        agent.load_document([document_path])
        return "Document loaded successfully."
    
    # Define the Gradio interface
    iface = gr.Interface(
        fn   = {
            "run_action": run_action,
            "load_document": load_document
        },
        inputs      = [
            gr.components.Dropdown(choices=["summarize", "search"], label="Action Type"),
            gr.components.Textbox(lines=5, label="Action Input"),
            gr.components.Textbox(label="Document Path")
        ],
        outputs     = "text",
        title       = "Agent Bean Interface",
        description = "Select an action and provide the input for the action. Then press Run to execute the action. You can also load a document into the vectorstore by providing its path and pressing Load Document."
    )
    
    # Launch the interface
    iface.launch()
    =======
    import json
    
    import gradio.blocks as gr
    
    from agent_bean import AgentBean
    
    # Load the settings json file and create a AgentBean object
    with open('settings.json') as f:
        setup = json.load(f)
    agent = AgentBean(setup['AgentBean_settings'])
    
    # Define the function to be called when the Run button is pressed
    def run_action(action_type, action_input):
        return agent.agent_action(action_type, [action_input])
    
    # Define the function to be called when the Load Document button is pressed
    def load_document(document_path):
        agent.load_document([document_path])
        return "Document loaded successfully."
    
    # Define the Gradio interface
    iface = gr.Blocks(
        blocks=[
            gr.Block('run_action', run_action, inputs=[
                gr.Dropdown(choices=["summarize", "search"], label="Action Type"),
                gr.Textbox(lines=5, label="Action Input"),
            ]),
            gr.Block('load_document', load_document, inputs=[
                gr.Textbox(label="Document Path")
            ]),
        ],
        outputs="text",
        title="Agent Bean Interface",
        description="Select an action and provide the input for the action. Then press Run to execute the action. You can also load a document into the vectorstore by providing its path and pressing Load Document."
    )
    
    # Launch the interface
    iface.launch()
    >>>>>>> updated
    ```
    Please note that `gr.Blocks` is not a direct replacement for `gr.Interface`. The `gr.Blocks` class is used to create a more complex interface with multiple inputs and outputs. If you only need a simple interface with a single input and output, you should continue using `gr.Interface`.

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -1,13 +1,12 @@
 import json
 
-import gradio       as     gr
+import gradio.blocks as gr
 
-from   agent_bean   import AgentBean
-
+from agent_bean import AgentBean
 
 # Load the settings json file and create a AgentBean object
 with open('settings.json') as f:
-    setup  = json.load(f)
+    setup = json.load(f)
 agent = AgentBean(setup['AgentBean_settings'])
 
 # Define the function to be called when the Run button is pressed
@@ -20,19 +19,19 @@ def load_document(document_path):
     return "Document loaded successfully."
 
 # Define the Gradio interface
-iface = gr.Interface(
-    fn   = {
-        "run_action": run_action,
-        "load_document": load_document
-    },
-    inputs      = [
-        gr.components.Dropdown(choices=["summarize", "search"], label="Action Type"),
-        gr.components.Textbox(lines=5, label="Action Input"),
-        gr.components.Textbox(label="Document Path")
+iface = gr.Blocks(
+    blocks=[
+        gr.Block('run_action', run_action, inputs=[
+            gr.Dropdown(choices=["summarize", "search"], label="Action Type"),
+            gr.Textbox(lines=5, label="Action Input"),
+        ]),
+        gr.Block('load_document', load_document, inputs=[
+            gr.Textbox(label="Document Path")
+        ]),
     ],
-    outputs     = "text",
-    title       = "Agent Bean Interface",
-    description = "Select an action and provide the input for the action. Then press Run to execute the action. You can also load a document into the vectorstore by providing its path and pressing Load Document."
+    outputs="text",
+    title="Agent Bean Interface",
+    description="Select an action and provide the input for the action. Then press Run to execute the action. You can also load a document into the vectorstore by providing its path and pressing Load Document."
 )
 
 # Launch the interface
