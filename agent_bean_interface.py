85dd198e618a9d284c6e193e9fe7f471b546ebaf
kolergy <jmrog@free.fr>
2023-08-30 14:05:49 +0200

    Refactored the Gradio interface to use the new gradio API and added buttons for running actions and loading documents.

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -1,6 +1,6 @@
 import json
 
-import gradio.inputs as gr
+import gradio as gr
 
 from agent_bean import AgentBean
 
@@ -18,21 +18,19 @@ def load_document(document_path):
     agent.load_document([document_path])
     return "Document loaded successfully."
 
-# Define the Gradio interface
-iface = gr.Blocks(
-    blocks=[
-        gr.Block('run_action', run_action, inputs=[
-            gr.Dropdown(choices=["summarize", "search"], label="Action Type"),
-            gr.Textbox(lines=5, label="Action Input"),
-        ]),
-        gr.Block('load_document', load_document, inputs=[
-            gr.Textbox(label="Document Path")
-        ]),
-    ],
-    outputs="text",
-    title="Agent Bean Interface",
-    description="Select an action and provide the input for the action. Then press Run to execute the action. You can also load a document into the vectorstore by providing its path and pressing Load Document."
-)
+# Define the Gradio display
+with gr.Blocks(title="Agent Bean Interface") as iface:
+    action_type          = gr.inputs.Dropdown(choices = ["summarize", "search"], label = "Action Type" ) 
+    action_input         = gr.inputs.Textbox( lines   = 5,                       label = "Action Input")
+    run_button           = gr.Button(  label   = "Run"          )
+    document_path        = gr.inputs.File(    label   = "Document Path")
+    load_document_button = gr.Button(  label   = "Load Document")
+    text                 = gr.outputs.Textbox(label   = "Output Text"  )
+
+    run_button.click(run_action, [action_type, action_input], outputs = text)
+    load_document_button.click(load_document, [document_path], outputs = text)
+   
+
 
 # Launch the interface
 iface.launch()
