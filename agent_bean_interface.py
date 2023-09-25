31f69fb006161c984e64a645ccc3823fcc0d0928
kolergy <jmrog@free.fr>
2023-09-25 09:25:09 +0200

    improve output display

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -16,30 +16,32 @@ agent = AgentBean(setup)
 
 # Define the function to be called when the Run button is pressed
 def run_action(action_type, action_input):
-    return agent.agent_action(action_type, [action_input])[0]
+    output = ''.join(agent.agent_action(action_type, [action_input]))
+    print(f"LEN OUTPUT: {len(output)}")
+    return output
 
 
 # Define the function to get system information
 def get_system_info():
-    cpu_brand      = agent.system_info.get_cpu_brand()
-    cpu_cores      = agent.system_info.get_cpu_cores()
-    ram_total      = agent.system_info.get_ram_total()
-    ram_used       = agent.system_info.get_ram_used()
-    gpu_brand      = agent.system_info.get_gpu_brand()
-    vram_total     = agent.system_info.get_vram_total()
-    vram_used      = agent.system_info.get_vram_used()
-    vram_available = agent.system_info.get_vram_available()
+    cpu_brand      = agent.system_info.get_cpu_brand(      )
+    cpu_cores      = agent.system_info.get_cpu_cores(      )
+    ram_total      = agent.system_info.get_ram_total(      )
+    ram_used       = agent.system_info.get_ram_used(       )
+    gpu_brand      = agent.system_info.get_gpu_brand(      )
+    vram_total     = agent.system_info.get_vram_total(     )
+    vram_used      = agent.system_info.get_vram_used(      )
+    vram_available = agent.system_info.get_vram_available( )
     return f"CPU Brand: {cpu_brand}\nCPU Cores: {cpu_cores}\nRAM Total: {ram_total} GB\nRAM Used: {ram_used} GB\nGPU Brand: {gpu_brand}\nVRAM Total: {vram_total} GB\nVRAM Used: {vram_used} GB\nVRAM Available: {vram_available} GB"
 
 
 # Define the Gradio display
 with gr.Blocks(title="Agent Bean Interface") as iface:
-    action_type          = gr.components.Dropdown(choices = agent.aa.get_available_actions(), label = "Action Type" ) 
-    action_input         = gr.components.Textbox( lines   = 5,                       label = "Action Input")
-    run_button           = gr.Button(             label   = "Run"          )
-    text_output          = gr.components.Textbox( label   = "Output Text"  )
+    action_type          = gr.components.Dropdown(choices = agent.aa.get_available_actions(), label = "Action Type"  ) 
+    action_input         = gr.components.Textbox( lines   = 5                               , label = "Action Input" )
+    run_button           = gr.Button(                                                         label = "Run"          )
+    text_output          = gr.components.Textbox( lines   = 20, autoscroll = True           , label = "Output Text"  )
 
-    run_button.click( run_action   , [action_type, action_input], outputs = text_output)
+    run_button.click( run_action, [action_type, action_input], outputs = text_output)
 
 # Launch the interface
 iface.launch(share=True)
