dabd373fb4c005e94e37bbc467eceffd772729ba
Kolergy <kolergy@google.com>
2023-09-26 06:38:59 +0200

    cln

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -1,8 +1,6 @@
 import json
 import time
 
-import numpy                 as     np 
-import matplotlib.pyplot     as     plt
 import pandas                as     pd
 import gradio                as     gr
 
