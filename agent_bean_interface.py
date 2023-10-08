ffaf926fd9fcc1e1b4af6df48acea0b24df7b34b
kolergy <jmrog@free.fr>
2023-10-08 23:15:01 +0200

    New actions implemented

--- agent_bean_interface.py
+++ agent_bean_interface.py
@@ -7,8 +7,8 @@ import gradio                as     gr
 from   agent_bean.agent_bean import AgentBean
 
 
-settings_file  = 'settings_opai.json'
-#settings_file  = 'settings_trans.json'
+#settings_file  = 'settings_opai.json'
+settings_file  = 'settings_trans.json'
 
 ram_total_Gb   = 0.0
 v_ram_total_Gb = 0.0
@@ -38,9 +38,9 @@ v_ram_label    = f"GPU: {gpu_brand}, VRAM Total: {v_ram_total_Gb:6.2f} Gb, VRAM 
 
 
 # Define the function to be called when the Run button is pressed
-def run_action(action_type, action_input):
+def run_action(action_name, action_input):
     """ Run the action: using the configured llm agent"""
-    output = ''.join(agent.agent_action(action_type, [action_input]))
+    output = ''.join(agent.agent_action(action_name, [action_input]))
     #print(f"LEN OUTPUT: {len(output)}")
     return output
 
@@ -53,7 +53,7 @@ def update_ram():
     time_s.append(       time.time() - start_time  )
 
     df         = pd.DataFrame({'time_s': time_s, 'ram_used_Gb': ram_used_Gb, 'v_ram_used_Gb': v_ram_used_Gb})
-    update_ram = gr.LinePlot.update(
+    update_ram = gr.LinePlot(
                         value  = df, 
                         title  = ram_label, 
                         x      = 'time_s', 
@@ -68,7 +68,7 @@ def update_v_ram():
     """ Update the v_ram plot """
     #print(f"update_v_ram() called, elapsed: {time.time() - start_time:6.2f} s. v_ram_used_Gb: {agent.si.get_v_ram_used():6.2f} Gb. v_ram_total_Gb: {agent.si.get_v_ram_total():6.2f} Gb. v_ram_free_Gb: {agent.si.get_v_ram_free():6.2f} Gb.")
     df         = pd.DataFrame({'time_s': time_s, 'ram_used_Gb': ram_used_Gb, 'v_ram_used_Gb': v_ram_used_Gb})  
-    update_v_ram = gr.LinePlot.update(
+    update_v_ram = gr.LinePlot(
                         value  = df, 
                         title  = v_ram_label, 
                         x      = 'time_s', 
@@ -82,14 +82,14 @@ def update_v_ram():
 
 # Define the Gradio display
 with gr.Blocks(title="Agent Bean Interface") as iface:
-    action_type          = gr.components.Dropdown(choices = agent.aa.get_available_actions(), label = "Action Type"   ) 
+    action_name          = gr.components.Dropdown(choices = agent.aa.get_available_actions(), label = "Action Name"   ) 
     action_input         = gr.components.Textbox( lines   = 5                               , label = "Action Input"  )
     run_button           = gr.Button(                                                         label = "Run"           )
     text_output          = gr.components.Textbox( lines   = 20,  autoscroll = True          , label = "Output Text"   )
     ram_plt              = gr.components.LinePlot(show_label=False)
     v_ram_plt            = gr.components.LinePlot(show_label=False)
 
-    run_button.click( run_action, [action_type, action_input], outputs = text_output)
+    run_button.click( run_action, [action_name, action_input], outputs = text_output)
 
     dep_ram              = iface.load(update_ram  , None, ram_plt  , every=1)
     dep_v_ram            = iface.load(update_v_ram, None, v_ram_plt, every=1)
