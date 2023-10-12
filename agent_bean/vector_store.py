53478eda93765c2b40cb74aadcf86aff4d957623
kolergy <jmrog@free.fr>
2023-10-12 18:17:47 +0200

    verctor store

---
+++ agent_bean/vector_store.py
@@ -0,0 +1,9 @@
+# Define the VectorStore class
+# allows to store text in a vectorstore
+#  - from the user side
+#  - from the generated side
+#  - from uploading documents (pdf, doc, txt, ps, latex, rtf, html, etc.)
+#  - from scraping web pages
+#
+# allows to retrieve text from the vectorstore
+
